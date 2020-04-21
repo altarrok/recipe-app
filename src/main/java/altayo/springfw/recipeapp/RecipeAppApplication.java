fa31accab89865635c694c07098e2ed28ed4fe15
altay batuhan <altaybatuhanmail@gmail.com>
2020-04-21 14:34:14 -0700

    Init add

---
+++ src/main/java/altayo/springfw/recipeapp/RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package altayo.springfw.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeAppApplication.class, args);
+    }
+
+}
