47a965965492e7dcb1ca7eb7db439f7f72e81f5b
altay batuhan <altaybatuhanmail@gmail.com>
2020-05-01 13:29:47 -0700

    Added Recipe service & impl

---
+++ src/main/java/altayo/springfw/recipeapp/services/RecipeService.java
@@ -0,0 +1,9 @@
+package altayo.springfw.recipeapp.services;
+
+import altayo.springfw.recipeapp.models.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+    Set<Recipe> getRecipes();
+}
