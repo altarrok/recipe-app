3a4eee78b4d9c83e962bd2483de3b404e910058c
altay batuhan <altaybatuhanmail@gmail.com>
2020-05-01 13:30:34 -0700

    Edit index page to show list of receipts

--- src/main/java/altayo/springfw/recipeapp/controllers/IndexController.java
+++ src/main/java/altayo/springfw/recipeapp/controllers/IndexController.java
@@ -1,26 +1,22 @@
 package altayo.springfw.recipeapp.controllers;
 
-import altayo.springfw.recipeapp.repositories.CategoryRepository;
-import altayo.springfw.recipeapp.repositories.RecipeRepository;
-import altayo.springfw.recipeapp.repositories.UnitOfMeasureRepository;
+import altayo.springfw.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-    private final CategoryRepository categoryRepository;
-    private final UnitOfMeasureRepository unitOfMeasureRepository;
-    private final RecipeRepository recipeRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository, RecipeRepository recipeRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
-        this.recipeRepository = recipeRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping("/")
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
