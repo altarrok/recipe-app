e31474ba46d3bed687fbd1b465d82726b7879272
altay batuhan <altaybatuhanmail@gmail.com>
2020-05-05 10:38:22 -0700

    Added Unit tests for Category

--- src/test/java/altayo/springfw/recipeapp/models/CategoryTest.java
+++ src/test/java/altayo/springfw/recipeapp/models/CategoryTest.java
@@ -3,6 +3,9 @@ package altayo.springfw.recipeapp.models;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CategoryTest {
@@ -23,9 +26,22 @@ class CategoryTest {
 
     @Test
     void getDescription() {
+        String descValue = "Description";
+        category.setDescription(descValue);
+        assertEquals(descValue, category.getDescription());
     }
 
     @Test
     void getRecipes() {
+        Set<Recipe> recipeSet = new HashSet<>();
+        Recipe recipe1 = new Recipe();
+        Recipe recipe2 = new Recipe();
+        recipe1.setId(1L);
+        recipe2.setId(2L);
+
+        category.getRecipes().add(recipe1);
+        category.getRecipes().add(recipe2);
+
+        assertEquals(2, category.getRecipes().size());
     }
 }
