750f340a771a38b95db132e6ce7bca11c2b5ed90
altay batuhan <altaybatuhanmail@gmail.com>
2020-05-07 10:12:46 -0700

    implemented Show methods

--- src/main/java/altayo/springfw/recipeapp/controllers/RecipeController.java
+++ src/main/java/altayo/springfw/recipeapp/controllers/RecipeController.java
@@ -3,9 +3,13 @@ package altayo.springfw.recipeapp.controllers;
 import altayo.springfw.recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
+@RequestMapping("/recipe")
 public class RecipeController {
     private final RecipeService recipeService;
 
@@ -13,7 +17,9 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    public String showRecipe() {
-        return null;
+    @RequestMapping("/{id}")
+    public String showRecipe(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findById(new Long(id)));
+        return "recipe/show";
     }
 }
--- src/main/java/altayo/springfw/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/altayo/springfw/recipeapp/services/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -29,6 +30,12 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Recipe findById(Long id) {
-        return null;
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+
+        if (!recipeOptional.isPresent()) {
+            throw new RuntimeException("Recipe Not Found");
+        }
+
+        return recipeOptional.get();
     }
 }
